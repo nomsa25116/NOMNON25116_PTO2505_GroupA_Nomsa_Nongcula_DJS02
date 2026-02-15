29407005fbc72c3aa0bfee9ef4c3fe4c14c09c60
nomsa25116 <ntnongcula@gmail.com>
2026-02-15 20:51:07 +0200

    add podcast-preview.js file and linked it in index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { podcasts } from "./data.js";
 import { createModal } from "./components/createModal.js";
 import { createGrid } from "./views/createGrid.js";
+import "./components/podcast-preview.js";
 
 /**
  * Initializes the podcast application.
